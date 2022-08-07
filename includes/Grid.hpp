b6d7688768bf552c76294a4d6ae7e691cd73e23b
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-08-07 09:08:56 +0200

    add doc to grid

--- includes/Grid.hpp
+++ includes/Grid.hpp
@@ -7,23 +7,97 @@
 #include <utility>
 #include "../includes/Tile.hpp"
 
-typedef std::pair<int, int> PVector2;
+/**
+ * @brief PVector2Grid is a Vector used in the pacman grids.
+ * 
+ * the first value is used to initialize the row, and the second for the columns.w
+ * 
+ */
+typedef std::pair<int, int> PVector2Grid;
 
+/**
+ * @brief Grid class, store the datas in matrix (normalGrid, and pacGumGrid). 
+ * 
+ */
 class Grid {
 
 public:
+
+    /**
+     * @brief Construct a new Grid object, initializes the grids.
+     * 
+     */
     Grid();
 
-    void initializeGrid();
-    //write a method to transform grid in Graph;
-    void get(PVector2 position);
-    void set(PVector2 position, int value);
-    int* wallToBin(PVector2 position);
-    bool isWallPresent(PVector2 position, Wall wall);
+    /**
+     * @brief method used to initialize the grids.
+     * 
+     */
+    void initializeGrids();
+
+    /**
+     * @brief getter to get the tile value in a specific row and a specific column.
+     * 
+     * @param position position in PVector2Grid (row, col)
+     */
+    void get(PVector2Grid position) const;
+
+    /**
+     * @brief setter to set the tile value in a specific row and a specific column.
+     * 
+     * @param position position in PVector2Grid (row, col)
+     * 
+     * @param value the new m_value.
+     */
+    void set(PVector2Grid position, int value);
+
+    /**
+     * @brief convert m_value of a specific tile in a reversed array binary
+     * 
+     * @param position position in PVector2Grid (row, col)
+     * 
+     * @return int* the array
+     */
+    int* wallToBin(PVector2Grid position);
+
+    /**
+     * @brief check if a specific wall is present in a tile value
+     * 
+     * @param position position in PVector2Grid (row, col)
+     * 
+     * @param wall the wall reached.
+     * 
+     * @return true if the wall is present
+     * 
+     * @return false if the wall is not present
+     */
+    bool isWallPresent(PVector2Grid position, Wall wall);
     
 private:
 
-    int grid[GRID_HEIGHT][GRID_WIDTH];
+    /**
+     * @brief initialize main grid
+     * 
+     */
+    void initializeMainGrid();
+
+    /**
+     * @brief converts the main grid in a graph.
+     * 
+     */
+    void initializeGraphGrid();
+
+    /**
+     * @brief initialize the grid with the pacGum values.
+     * 
+     */
+    void initializePacGumGrid();
+
+    /**
+     * @brief main grid.
+     * 
+     */
+    Tile grid[GRID_HEIGHT][GRID_WIDTH];
 
 };
 
