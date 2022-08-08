2fdd336b1dbc4193425475edd3f883a5274ae80c
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-08-08 08:53:15 +0200

    add doc at pacgum

--- includes/PacGum.hpp
+++ includes/PacGum.hpp
@@ -1,12 +1,32 @@
 #ifndef PACGUM_HPP
 #define PACGUM_HPP
 
+/**
+ * @brief enum defines the differents types of pacman.
+ * 
+ *  - 0 is considered as empty 
+ *  - 1 as dot (normal pac gum)
+ *  - 2 as energizer (super pac gum)
+ * 
+ */
 enum class PacGum : int {
 
+    /**
+     * @brief empty pac gum
+     * 
+     */
     EMPTY = 0,
 
+    /**
+     * @brief normal pac gum
+     * 
+     */
     DOT = 1,
 
+    /**
+     * @brief super pac gum
+     * 
+     */
     ENERGIZER = 2
 
 };
