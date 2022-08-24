e6b633c38ba434aa6dac963ce389f84ef353bcc2
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-08-24 18:00:46 +0200

    add Target enum

---
+++ includes/Target.hpp
@@ -0,0 +1,31 @@
+#ifndef TARGET_HPP
+#define TARGET_HPP
+
+
+/**
+ * @brief defines the target to trap pacman
+ * 
+ */
+enum class Target {
+
+    /**
+     * @brief if target is PLAYER then the ghost follows directly pacman.
+     * 
+     */
+    PLAYER,
+
+    /**
+     * @brief if target is NEXT_POS_PLAYER, the ghost follows the next node met by pacman.
+     * 
+     */
+    NEXT_POS_PLAYER,
+
+    /**
+     * @brief if target is RANDOM, the ghost follows a random direction.
+     * 
+     */
+    RANDOM
+
+};
+
+#endif //TARGET_HPP
